59be72b17ffb993356bfc1bb0e0fec2d88199370
Godyouknowstyle <natthanon9252548@gmail.com>
2023-08-23 15:07:42 +0700

    afrff

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
+    /*=========================== toggle icon navbar =============================================*/
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+};
+
+
+    /*=========================== scroll section active link =============================================*/
 let sections = document.querySelectorAll('section');
 let navLinks = document.querySelectorAll('header nav a');
 
