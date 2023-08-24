249500d916061e5d6dfd2a69ead066fa62d48e46
Godyouknowstyle <natthanon9252548@gmail.com>
2023-08-24 12:13:13 +0700

    edit

--- script.js
+++ script.js
@@ -41,9 +41,10 @@ window.onscroll = () => {
 /*=========================== scroll reveal =============================================*/
 ScrollReveal({
     reset: true,
-    dsitance: '80px',
+    distance: '80px',
     duration: 2000,
     delay: 200
 });
 
 ScrollReveal().reveal('.home-content, .heading', { origin: 'top' });
+ScrollReveal().reveal('.home-img, .services-container, .portfolio-box, contsct form', { origin: 'bottom' });
