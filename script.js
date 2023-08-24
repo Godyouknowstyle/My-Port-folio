1e74bf22ca786192c85682fea2d1b126d1512fa3
Godyouknowstyle <natthanon9252548@gmail.com>
2023-08-24 12:36:54 +0700

    loop

--- script.js
+++ script.js
@@ -50,3 +50,13 @@ ScrollReveal().reveal('.home-content, .heading', { origin: 'top' });
 ScrollReveal().reveal('.home-img, .services-container, .portfolio-box, contsct form', { origin: 'bottom' });
 ScrollReveal().reveal('.home-content h1, .about-img', { origin: 'left' });
 ScrollReveal().reveal('.home-content p, .about-content', { origin: 'right' });
+
+
+/*=========================== typed js =============================================*/
+const typed = new Typed('.multiple-text', {
+    Strings: ['Frontend Developer', 'Game Player', 'Discord Developer',],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+});
