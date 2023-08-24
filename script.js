f89906f8f6be889c5ebee8533ca541abf3b98183
Godyouknowstyle <natthanon9252548@gmail.com>
2023-08-24 12:04:16 +0700

    delay

--- script.js
+++ script.js
@@ -36,3 +36,14 @@ window.onscroll = () => {
     menuIcon.classList.remove('bx-x');
     navbar.classList.remove('active');
 };
+
+
+/*=========================== scroll reveal =============================================*/
+ScrollReveal({
+    reset: true,
+    dsitance: '80px',
+    duration: 2000,
+    delay: 200
+});
+
+ScrollReveal().reveal('.home-content, .heading', { origin: 'top' });
