370d97d678fe9b173f69b4961d34410048a5f833
Godyouknowstyle <natthanon9252548@gmail.com>
2023-08-24 12:17:36 +0700

    afghj

--- script.js
+++ script.js
@@ -48,3 +48,5 @@ ScrollReveal({
 
 ScrollReveal().reveal('.home-content, .heading', { origin: 'top' });
 ScrollReveal().reveal('.home-img, .services-container, .portfolio-box, contsct form', { origin: 'bottom' });
+ScrollReveal().reveal('.home-content h1, .about-img', { origin: 'left' });
+ScrollReveal().reveal('.home-content p, .about-content', { origin: 'right' });
